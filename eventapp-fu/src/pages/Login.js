820da7af465d6baac661cafaf7e5f4cd3bc54627
trieu21320011 <74083815+trieu21320011@users.noreply.github.com>
2021-10-14 11:00:14 +0700

    Update Login.js

--- eventapp-fu/src/pages/Login.js
+++ eventapp-fu/src/pages/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     Grid,
     Box,
@@ -15,43 +15,69 @@ import {
 } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Form , Formik,Field } from 'formik';
+import { Form , Formik,Field, useFormik } from 'formik';
 import * as Yup from 'yup';
 import { ErrorMessage } from 'formik';
+import axios from 'axios';
+import { useHistory } from 'react-router';
+
 
 
 const theme = createTheme();
 
 export default function Login() {
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        // eslint-disable-next-line no-console
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
-    };
-    const initialValues={
-        email:'',
-        password:'',
-        remember:false
-    }
+  
+const initialValues= {
+    userName:'',
+    password:'',
+}
+const [message, setMessage] = useState("");
+const [user, setUser]= useState([]);
+const history= useHistory();
+
+
+//useEffect(() =>{
+    //if(localStorage.getItem('user-info')){
+    // history.push("/")
+   // }
+//}, [])
+
     const validationSchema=Yup.object().shape({
-        email:Yup.string().email('Please enter valid Email').required("Required Email"),
+        userName:Yup.string().email('Please enter valid Email').required("Required Email"),
         password:Yup.string().required("Required password ")
     })
-    const onSubmit= (values, props)=>{
-        console.log(values)
-    
-        setTimeout(()=>{
-            props.resetForm()
 
-        },2000)
+    const onSubmit = async (values, props) => {
+            console.log(values)
+            const result = await axios({
+            method: "POST",
+            url:'http://localhost:8000/user/checkLogin',
+            data: values,
+            withCredentials: 'include',
+            headers:{
+                "Content-Type": "application/json",
+                "Accept": 'application/json'
+            }
+        })
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
+          setUser(response.data);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+          if(user.length === 0){
+              setMessage("Invalid Email or Password")
+          }else{
+            localStorage.setItem("userInfo",JSON.stringify(user)) 
+            history.push("/")
+          }
+          
         
-
+        
     }
+ 
     
 
     return (
@@ -73,32 +99,32 @@ export default function Login() {
                         Login
                     </Typography>
                     <Box sx={{ mt: 1 }}>
-                   
+                    <Box  noValidate sx={{ mt: 1 }}> 
                         <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema} sx={{ mt: 1 }} >
                             {(props)=>(
-                                <Form>
+                                <Form>   
                                     {console.log(props)}
                                     <Field as={TextField}
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                            helperText={<ErrorMessage name="email"/>}
-                        />
-                        <Field as={TextField}
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                            helperText={<ErrorMessage name="password"/>}
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="userName"
+                                        label="Email Address"
+                                        name="userName"
+                                        autoComplete="userName"
+                                        autoFocus
+                                        helperText={<ErrorMessage name="userName" />}
+                                    />
+                                    <Field as={TextField}
+                                         margin="normal"
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="current-password"
+                                        helperText={<ErrorMessage name="password" />}
                         />
                         <Field as={ FormControlLabel}
                             name='remember'
@@ -111,15 +137,17 @@ export default function Login() {
                             variant="contained"
                             sx={{ mt: 3, mb: 2 }}
                             
+                            
                         >
                             Login
                         </Button>
+                        <div className="text-center">{message}</div>
                                 </Form>
 
                             )}
 
                         </Formik>
-
+                    </Box>          
                         <Grid container>
                             <Grid item xs style={{textAlign:"center"}}>
                                 <Link href="#" variant="body2">
